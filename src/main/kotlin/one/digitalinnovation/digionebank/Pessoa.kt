13db8fb85a8109fd92fe0f4b132dc3219d076412
plankkton <leonan_nogueira@hotmail.com>
2021-03-10 12:54:25 -0300

    atualização para aplicação do modificador "private" para setar função da propriedade CPF

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
 
-    var nome: String = "Sid"
+    var nome:String = "Sid"
     var cpf: String = "123.456.789-01"
-
+    private set
 }
 
 fun main(){
     val sid = Pessoa()
 
+    println(sid)
     println(sid.nome)
     println(sid.cpf)
 }
