f52ac4b505e732fcbd5295c8f6173a6b7f31abf5
plankkton <leonan_nogueira@hotmail.com>
2021-03-10 17:59:09 -0300

    atualização para abstrato nas classes pessoa e funcionário.

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,19 +1,9 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-
-    var nome:String = "Sid"
-    var cpf: String = "123.456.789-01"
-    private set
-
-    constructor()
-
-    fun pessoaInfo()="$nome e $cpf"
-}
-
-fun main(){
-    val sid = Pessoa()
-
-    println(sid.pessoaInfo())
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+    ){
 
 }
+
