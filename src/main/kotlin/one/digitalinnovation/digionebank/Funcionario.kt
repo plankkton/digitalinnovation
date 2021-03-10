c93051814697f8961e01a47568349142c97da943
plankkton <leonan_nogueira@hotmail.com>
2021-03-10 19:32:38 -0300

    criação de subclasses Analista e Gerente

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,12 +1,17 @@
 package one.digitalinnovation.digionebank
 
-import java.math.BigDecimal
-
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal
+    val salario: Double
 ) : Pessoa(nome,cpf) {
-    abstract fun calculoAuxilio();
+    abstract fun calculoAuxilio(): Double
+
+    override fun toString(): String = """
+        Nome: $nome
+        Cpf: $cpf
+        Salário: $salario
+        Auxílio: ${calculoAuxilio()}
+        """.trimIndent()
 
 }
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digionebank
+
+class Gerente
+    (nome:String, cpf:String, salario:Double)
+    :Funcionario(nome=nome, cpf=cpf, salario=salario) {
+    override fun calculoAuxilio(): Double =salario*0.4
+}
